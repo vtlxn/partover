9ba37811fde3b1e4564c390c3d71e31d3a9e7a7d
vtlxn <FaZa0837$!>
2019-03-25 22:34:21 +0200

    front1 beta4

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,23 @@
 'use strict';
 
-function classToggle() {
+function classToggleNav() {
   var navs = document.querySelectorAll('.navbar-items');
   navs.forEach(function (nav) {
     return nav.classList.toggle('navbar-items-show');
   });
 }
 
-document.querySelector('.menu-toggle').addEventListener('click', classToggle);
+document.querySelector('.menu-toggle').addEventListener('click', classToggleNav);
+
+function classToggleCurrency() {
+  var navs = document.querySelectorAll('.currency-dropdown');
+  navs.forEach(function (nav) {
+    return nav.classList.toggle('currency-dropdown-show');
+  });
+  var navs2 = document.querySelectorAll('.currency-arrow-dropdown');
+  navs2.forEach(function (nav) {
+    return nav.classList.toggle('currency-arrow-dropdown-show');
+  });
+}
+
+document.querySelector('.currency').addEventListener('click', classToggleCurrency);
--- src/script/main.js
+++ src/script/main.js
@@ -1,8 +1,19 @@
 'use strict';
 
-function classToggle() {
+function classToggleNav() {
     const navs = document.querySelectorAll('.navbar-items')
     navs.forEach(nav => nav.classList.toggle('navbar-items-show'));
 }
 
-document.querySelector('.menu-toggle').addEventListener('click', classToggle);
+document.querySelector('.menu-toggle').addEventListener('click', classToggleNav);
+
+
+function classToggleCurrency() {
+    const navs = document.querySelectorAll('.currency-dropdown')
+    navs.forEach(nav => nav.classList.toggle('currency-dropdown-show'));
+    
+    const navs2 = document.querySelectorAll('.currency-arrow-dropdown')
+    navs2.forEach(nav => nav.classList.toggle('currency-arrow-dropdown-show'));
+}
+
+document.querySelector('.currency').addEventListener('click', classToggleCurrency);
